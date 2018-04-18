fe51d0fb9df7bb2078326e75b89137f06b07ad28
juanigon4 <38008708+juanigon4@users.noreply.github.com>
2018-04-18 10:18:56 -0300

    Add files via upload

---
+++ 01-Suma.cpp/Suma.cpp
@@ -0,0 +1,15 @@
+/* Adición
+Suma de dos números
+JIG
+20180415
+*/
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin>> a;
+    int b;
+    std::cin>> b;
+    std::cout<< a+b;
+    return 0;
+}
